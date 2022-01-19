413799c9e522c024518941416e164d648877a999
Hide <padocon@naver.com>
2022-01-19 19:56:02 +0900

    feature: QueueMessageConfig 추가

---
+++ src/main/java/com/example/springcqrsexample/common/config/QueueMessageConfig.java
@@ -0,0 +1,50 @@
+package com.example.springcqrsexample.common.config;
+
+import com.amazonaws.services.sqs.AmazonSQSAsync;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.cloud.aws.messaging.config.QueueMessageHandlerFactory;
+import org.springframework.cloud.aws.messaging.config.SimpleMessageListenerContainerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.messaging.converter.MappingJackson2MessageConverter;
+import org.springframework.messaging.handler.annotation.support.PayloadMethodArgumentResolver;
+
+import java.util.Collections;
+
+@Configuration
+public class QueueMessageConfig {
+    @Bean
+    public SimpleMessageListenerContainerFactory simpleMessageListenerContainerFactory(AmazonSQSAsync amazonSQS) {
+        SimpleAsyncTaskExecutor simpleAsyncTaskExecutor = new SimpleAsyncTaskExecutor();
+        simpleAsyncTaskExecutor.setConcurrencyLimit(50);
+
+        SimpleMessageListenerContainerFactory factory = new SimpleMessageListenerContainerFactory();
+        factory.setAmazonSqs(amazonSQS);
+        factory.setMaxNumberOfMessages(10);
+        factory.setWaitTimeOut(10);
+        factory.setVisibilityTimeout(30);
+        factory.setTaskExecutor(simpleAsyncTaskExecutor);
+        return factory;
+    }
+
+    @Bean
+    public QueueMessageHandlerFactory queueMessageHandlerFactory(AmazonSQSAsync amazonSQS) {
+        MappingJackson2MessageConverter messageConverter = new MappingJackson2MessageConverter();
+        messageConverter.setStrictContentTypeMatch(false);
+        messageConverter.setObjectMapper(objectMapper());
+
+        QueueMessageHandlerFactory factory = new QueueMessageHandlerFactory();
+        factory.setAmazonSqs(amazonSQS);
+        factory.setArgumentResolvers(Collections.singletonList(new PayloadMethodArgumentResolver(messageConverter)));
+        return factory;
+    }
+
+    private ObjectMapper objectMapper() {
+        return Jackson2ObjectMapperBuilder.json()
+                .modules(new JavaTimeModule())
+                .build();
+    }
+}
