ec13916ce96b6d317c19b46df35a5ba49800c675
Hide <padocon@naver.com>
2022-01-20 22:10:19 +0900

    feature: Webclient 설치 및 M1 맥북 오류 대응

--- src/main/java/com/example/springcqrsexample/consumer/service/UserSyncService.java
+++ src/main/java/com/example/springcqrsexample/consumer/service/UserSyncService.java
@@ -6,8 +6,11 @@ import com.example.springcqrsexample.user.repository.UserRedisRepository;
 import com.example.springcqrsexample.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
 @Slf4j
@@ -16,6 +19,14 @@ import org.springframework.transaction.annotation.Transactional;
 public class UserSyncService {
     private final UserRepository userRepository;
     private final UserRedisRepository userRedisRepository;
+    private final String baseUrl = "http://localhost:8000";
+
+    private WebClient makeWebClient(String url, String contentType) {
+        return WebClient.builder()
+                .baseUrl(url)
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, contentType)
+                .build();
+    }
 
     private RedisUser makeRedisUser(User user) {
         return RedisUser.builder()
@@ -29,6 +40,8 @@ public class UserSyncService {
     }
 
     public void syncCreate(Long userId) {
+        WebClient webClient = makeWebClient(baseUrl, MediaType.APPLICATION_JSON_VALUE);
+        WebClient.ResponseSpec retrieve = webClient.get().retrieve();
         userRepository.findById(userId).ifPresent(user -> {
             RedisUser redisUser = makeRedisUser(user);
             userRedisRepository.save(redisUser);
@@ -37,6 +50,7 @@ public class UserSyncService {
     }
 
     public void syncUpdate(Long userId) {
+        WebClient webClient = makeWebClient(baseUrl, MediaType.APPLICATION_JSON_VALUE);
         userRepository.findById(userId).ifPresent(user -> {
             RedisUser redisUser = makeRedisUser(user);
             userRedisRepository.save(redisUser);
@@ -45,6 +59,7 @@ public class UserSyncService {
     }
 
     public void syncDelete(Long userId) {
+        WebClient webClient = makeWebClient(baseUrl, MediaType.APPLICATION_JSON_VALUE);
         userRedisRepository.deleteById(userId);
         log.info("Synchronize delete user");
     }
